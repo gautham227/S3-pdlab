591eee66d8da09719e190d070e512af0a03c71a8
G Gautham Krishna <ggauthamkrishna@gmail.com>
2021-10-04 20:11:59 +0530

    basic final 1

--- ASSG4_B200739CS_GAUTHAM_1.c
+++ ASSG4_B200739CS_GAUTHAM_1.c
@@ -3,6 +3,7 @@
 #include <string.h>
 #include <math.h>
 #include <stdlib.h>
+int cnt=0;
 
 int size(int arr[]){
     int s=0;
@@ -18,39 +19,43 @@ void swap(int arr[],int a,int b){
     arr[b]=temp;
 }
 
-int heapify(int arr[],int i,int larr,int cnt){
+void heapify(int arr[],int i,int larr){
     int largest=i;
     int lele=2*i+1;
     int rele=2*i+2;
     if(lele<larr && arr[lele]>arr[largest]){
+        cnt++;
         largest=lele;
     }
+    else if(lele<larr){
+        cnt++;
+    }
     if (rele<larr && arr[rele]>arr[largest]){
+        cnt++;
         largest=rele;
     }
-    cnt=cnt+2;
+    else if(rele<larr){
+        cnt++;
+    }
     if (largest!=i){
         swap(arr,i,largest);
-        heapify(arr,largest,larr,cnt);
+        heapify(arr,largest,larr);
     }
-    return cnt;
 }
 
-int mhp(int arr[],int n,int cnt){
+void mhp(int arr[],int n){
     for (int i = n/2-1; i >= 0; i--)
     {
-        cnt=cnt+heapify(arr,i,n,0);
+        heapify(arr,i,n);
     }
-    return cnt;
 }
 
-int heapsort(int arr[],int n){
-    int cnt=mhp(arr,n,cnt);
+void heapsort(int arr[],int n){
+    mhp(arr,n);
     for(int i=n-1;i>-1;i--){
         swap(arr,0,i);
-        cnt=heapify(arr,0,i,cnt);
+        heapify(arr,0,i);
     }
-    return cnt;
 }
 
 int main(){
@@ -62,7 +67,7 @@ int main(){
         scanf("%d",&k);
         arr[i]=k;
     }
-    int cnt=heapsort(arr,n);
+    heapsort(arr,n);
     for(int i=0;i<n;i++){
         printf("%d ",arr[i]);
     }
