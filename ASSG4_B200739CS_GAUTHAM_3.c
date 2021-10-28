1278c7f38199eea5cc46e859ae4e1707e002eb17
G Gautham Krishna <ggauthamkrishna@gmail.com>
2021-10-28 09:54:55 +0530

    my final

--- ASSG4_B200739CS_GAUTHAM_3.c
+++ ASSG4_B200739CS_GAUTHAM_3.c
@@ -1,144 +1,118 @@
-#include<stdio.h>
-#include<math.h>
-#include<string.h>
-#include<stdlib.h>
-    struct amoeba{
-        char amoeba_name[25];
-        int amoeba_size;
-    };
+//done by gautham on 30/9
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
 
+struct amoeba{
+    char amoeba_Name[21];
+    int amoeba_size;
+};
 
-    
-    void Compare(struct amoeba* A, int n){
-    struct amoeba temp;
-        for(int i=1;i<=n;i++)
-        {
-        if(A[0].amoeba_size==A[i].amoeba_size && (strcmp(A[0].amoeba_name,A[i].amoeba_name))>0){
-        temp=A[0];
-        A[0]=A[i];
-        A[i]=temp;
+void swap(struct amoeba* arr,int a,int b){
+    struct amoeba temp=arr[a];
+    arr[a]=arr[b];
+    arr[b]=temp;
+}
+
+void heapify(struct amoeba* arr,int i,int larr){
+    int largest=i;
+    int lele=2*i+1;
+    int rele=2*i+2;
+    if(lele<larr && arr[lele].amoeba_size>arr[largest].amoeba_size){
+        largest=lele;
+    }
+    if (rele<larr && arr[rele].amoeba_size>arr[largest].amoeba_size){
+        largest=rele;
+    }
+    if (largest!=i){
+        swap(arr,i,largest);
+        heapify(arr,largest,larr);
+    }
+}
+
+void mhp(struct amoeba* arr,int n){
+    for (int i = (n)/2-1; i >= 0; i--)
+    {
+        heapify(arr,i,n);
+    }
+}
+
+struct amoeba* srtname(struct amoeba* arr,int n){
+    for(int i=1;i<n;i++){
+        if (arr[0].amoeba_size==arr[i].amoeba_size && strcmp(arr[0].amoeba_Name,arr[i].amoeba_Name)>0){
+            swap(arr,0,i);
         }
     }
-    }
+    return arr;
+}
 
-    void max_heapify(struct amoeba* A,int i,int n){
-    int l,r;
-    int lex_check=0;
-    int lex_check1=0;
-    int smallest=i;
-    l=(2*i)+1;
-    r=(2*i)+2;
-    
-    if(l<=n && A[l].amoeba_size>A[i].amoeba_size) 
-    {
-    smallest=l;
+struct amoeba READ(struct amoeba ele){
+    scanf("%s ",&ele.amoeba_Name);
+    int k;
+    scanf("%d",&k);
+    ele.amoeba_size=k*(-1);
+    return ele;
+}
 
-    }
-    if(r<=n && A[r].amoeba_size>A[smallest].amoeba_size)  
-    {
-        smallest=r;
-    }
-    
-    struct amoeba temp;
+struct amoeba* INSERT(struct amoeba* arr,struct amoeba ele,int n){
+    strcpy(arr[n].amoeba_Name,ele.amoeba_Name);
+    arr[n].amoeba_size=ele.amoeba_size;
+    return arr;
+}
 
-    if(smallest!=i){
-        temp=A[i];
-        A[i]=A[smallest];
-        A[smallest]=temp;
-        max_heapify(A,smallest,n);
-        }
+struct amoeba* hpinsert(struct amoeba* arr,struct amoeba ele,int len){
+    strcpy(arr[len].amoeba_Name,ele.amoeba_Name);
+    arr[len].amoeba_size=ele.amoeba_size;
+    int i=len;
+    while(i>=0 && arr[i/2].amoeba_size<arr[i].amoeba_size){
+        swap(arr,i,i/2);
+        i=i/2;
+    }
+    srtname(arr,len+1);
+    return arr;
+}
 
-    }
-    
-    void build_heap(struct amoeba*A,int n)
-    {
-    for(int i=(n/2);i>=0;i--)
-    {
-        max_heapify(A,i,n);
-    }
-    }
-    
-    void heap_sort(struct amoeba*A, int n){
-        int n1=n;
-        int heapsize=n;
-        build_heap(A,n);
-        
-        struct amoeba temp;
-        int j;
-        for(j=n1;j>=1;j--)
-        {
-        temp=A[j];
-        A[j]=A[0];
-        A[0]=temp;
-        heapsize--;
-        max_heapify(A,0,heapsize);
-        }
+struct amoeba* hpdelete(struct amoeba* arr,int len){
+    swap(arr,0,len-1);
+    heapify(arr,0,len-1);
+    srtname(arr,len-1);
+    return arr;
+}
 
+struct amoeba* COMBINE(struct amoeba* arr,int n){
+    struct amoeba ans;
+    while(n!=1){
+        strcpy(ans.amoeba_Name,arr[0].amoeba_Name);
+        ans.amoeba_size=arr[0].amoeba_size;
+        printf("%s ",arr[0].amoeba_Name);
+        arr=hpdelete(arr,n);
+        n--;
+        ans.amoeba_size=ans.amoeba_size+arr[0].amoeba_size;
+        printf("%s ",arr[0].amoeba_Name);
+        arr=hpdelete(arr,n);
+        n--;
+        arr=hpinsert(arr,ans,n);
+        n++;
+        printf("%d",ans.amoeba_size*(-1));
+        printf("\n");
     }
-
-    void insert(struct amoeba* A,int count){
-        heap_sort(A,count);
-        Compare(A,count);
-        
+    printf("%s ",arr[0].amoeba_Name);
+    printf("%d\n",arr[0].amoeba_size*(-1));
+    return  arr;
+}
+int main(){
+    struct amoeba* arr;
+    struct amoeba ele;
+    int n;
+    scanf("%d",&n);
+    arr=(struct amoeba*)malloc(n*sizeof(struct amoeba));
+    for(int i=0;i<n;i++){
+        ele=READ(ele);
+        arr=INSERT(arr,ele,i);
     }
-
-    void read(struct amoeba* A,int count){
-        for(int i=0;i<=count;i++)
-        {
-            scanf("%s",(A+i)->amoeba_name);
-            scanf("%d",&(A+i)->amoeba_size);
-            
-        }
-    }
-
-    void deleteelement(struct amoeba* A,int len){
-        struct amoeba temp;
-        temp=A[0];
-        A[0]=A[len];
-        A[len]=temp;
-        heap_sort(A,len-1);
-        Compare(A,len-1);
-    } 
-    
-    void appendelement(struct amoeba* A,struct amoeba xyz, int len){
-        strcpy(A[len].amoeba_name,xyz.amoeba_name);
-        A[len].amoeba_size=xyz.amoeba_size;
-        heap_sort(A,len);
-        Compare(A,len);
-    }
-
-    void combine(struct amoeba* A, int n)
-    {   
-        while(n>0){
-            struct amoeba rithika;
-            strcpy(rithika.amoeba_name,A[0].amoeba_name);
-            rithika.amoeba_size=A[0].amoeba_size;
-            printf("%s ",A[0]);
-            deleteelement(A,n);
-            n--;
-            printf("%s ",A[0]);
-            rithika.amoeba_size=rithika.amoeba_size+A[0].amoeba_size;
-            deleteelement(A,n);
-            n--;
-            appendelement(A,rithika,n+1);
-            n++;
-            heap_sort(A,n);
-            Compare(A,n);
-            printf("%d",rithika.amoeba_size);
-            printf("\n");
-        }
-        printf("%s ",A[0].amoeba_name);
-        printf("%d\n",A[0].amoeba_size);
-    }
-    int main()
-    {
-        int n;
-        scanf("%d",&n);
-        struct amoeba* Array_of_amoeba;
-        Array_of_amoeba = (struct amoeba*)malloc(n * sizeof(struct amoeba));
-        read(Array_of_amoeba,n-1);
-        heap_sort(Array_of_amoeba,n-1);
-        Compare(Array_of_amoeba,n-1);
-        combine(Array_of_amoeba,n-1);
-        return 0;
-    }
+    mhp(arr,n);
+    arr=srtname(arr,n);
+    COMBINE(arr,n);
+    return 0;
+}
